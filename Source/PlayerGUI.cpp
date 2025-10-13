08c3b6c85f62f250357a36e5b178859a9e2d27ec
birdbox219 <mahmodelsayed882@gmail.com>
2025-10-13 11:23:15 +0300

    Refactored Maincomp and seprated GUI from logic

---
+++ Source/PlayerGUI.cpp
@@ -0,0 +1,51 @@
+﻿#include "PlayerGUI.h"
+
+PlayerGUI::PlayerGUI()
+{
+    //for (auto* btn : { &loadButton, &playButton , &stopButton })
+    //{
+    //    //btn->addListener(this);
+    //    addAndMakeVisible(btn);
+    //}
+
+
+
+    addAndMakeVisible(loadButton);
+    addAndMakeVisible(playButton);
+    addAndMakeVisible(stopButton);
+    
+
+
+
+
+    // Volume slider
+    volumeSlider.setRange(0.0, 1.0, 0.01);
+    volumeSlider.setValue(0.5);
+    //volumeSlider.addListener(this);
+    addAndMakeVisible(volumeSlider);
+
+    //setSize(500, 250);
+    //setAudioChannels(0, 2);
+}
+
+
+PlayerGUI::~PlayerGUI() {}
+
+
+void PlayerGUI::paint(juce::Graphics& g)
+{
+    g.fillAll(juce::Colours::blueviolet);
+}
+
+void PlayerGUI::resized()
+{
+    int y = 20;
+    loadButton.setBounds(20, y, 100, 40);
+    playButton.setBounds(140, y, 80, 40);
+    stopButton.setBounds(240, y, 80, 40);
+    /*prevButton.setBounds(340, y, 80, 40);
+    nextButton.setBounds(440, y, 80, 40);*/
+
+    volumeSlider.setBounds(20, 100, getWidth() - 40, 30);
+}
+
---
+++ Source/PlayerGUI.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+# include "JuceHeader.h"
+
+class PlayerGUI : public juce::Component
+
+{
+public:
+	PlayerGUI();
+	~PlayerGUI() override;
+
+    void paint(juce::Graphics& g) override;
+    void resized() override;
+
+    //void buttonClicked(juce::Button* button) override;
+    //void sliderValueChanged(juce::Slider* slider) override;
+    juce::TextButton loadButton{ "Load Files" };
+    //juce::TextButton restartButton{ "Restart" };
+    juce::TextButton playButton{ "Play" };
+    juce::TextButton stopButton{ "Stop" };
+    juce::Slider volumeSlider;
+
+   
+
+
+private:
+    
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI)
+
+
+};
