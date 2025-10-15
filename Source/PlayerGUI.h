e02ec9c51c481d84ca1bcce30c63b5b14200c268
alaa <lolomamdo2006@gmail.com>
2025-10-15 23:41:57 +0300

    Added  duration

--- Source/PlayerGUI.h
+++ Source/PlayerGUI.h
@@ -10,7 +10,7 @@ public:
 
     void paint(juce::Graphics& g) override;
     void resized() override;
-    void metaData(juce::String& fileName);
+    void metaData(juce::String& fileName,double&totalTime);
 
     //void buttonClicked(juce::Button* button) override;
     //void sliderValueChanged(juce::Slider* slider) override;
@@ -59,6 +59,7 @@ public:
 
     // Lebels
     juce::Label title;
+    juce::Label time;
 
     juce::Label currentTimeLabel;
     juce::Label TotalTimeLabel;
